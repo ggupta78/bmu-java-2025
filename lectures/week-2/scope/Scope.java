6ada29d3cd00ae4d414562398b6da9ecce547d60
Gautam Gupta <ggupta78@gmail.com>
2025-08-13 08:27:07 +0530

    update scope example

--- lectures/week-2/scope/Scope.java
+++ lectures/week-2/scope/Scope.java
@@ -1,9 +1,9 @@
 public class Scope {
-  private int instanceVariable = 4; //class instance scope
+  public int instanceVariable = 4; // class instance scope
 
   public int getInstanceVariable() {
 
-    if (true) { //block scope
+    if (true) { // block scope
       int instanceVariable = 6;
       System.out.println("Inside if block, instanceVariable value is: " + instanceVariable);
     }
@@ -11,5 +11,5 @@ public class Scope {
     return this.instanceVariable;
   }
 
-  static String staticVariable = "Class Level"; //class scope, available without instance
+  static String staticVariable = "Class Level"; // class scope, available without instance
 }
--- lectures/week-2/scope/ScopeTest.java
+++ lectures/week-2/scope/ScopeTest.java
@@ -1,17 +1,30 @@
 public class ScopeTest {
   public static void main(String[] args) {
-    //Test printing the static variable in Scope class
+    // Test printing the static variable in Scope class
+    System.out.println(Scope.staticVariable);
 
+    // Test calling the getInstanceVariable() method and print its result
+    Scope scope = new Scope();
+    System.out.println(scope.getInstanceVariable());
 
-    //Test calling the getInstanceVariable() method and print its result
-
-
-    //Test method scope
-
+    // Test method scope
+    int anyNumber = 3;
+    String str = "Hello";
+    methodScope(str);
+    // System.out.println(anyNumber);
+    // System.out.println(scope.instanceVariable);
+    System.out.println(str);
   }
 
-  private static void methodScope(int anyNumber) {
-    System.out.println("Anynumber: " + anyNumber);
+  private static void methodScope(String str1) {
+    // System.out.println("Anynumber: " + anyNumber);
+    // anyNumber = 7;
+
+    System.out.println(str1);
+    str1 = "Hello World!";
+
+    // System.out.println(scope.instanceVariable);
+    // scope.instanceVariable = 7;
 
     double PI = 3.14;
     System.out.println("PI: " + PI);
